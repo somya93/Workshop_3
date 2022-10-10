a7533d19207c213d80b95166398c6a632350e666
somya93 <somya93@gmail.com>
2022-10-10 15:58:46 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Workshop_3
+# Workshop 3
+
+In this workshop, we'll be extending our previous project into an API.
+
+Make sure to have these dependencies added to your project interpreter in PyCharm.
+
+ 1. [Flask](https://flask.palletsprojects.com/en/1.1.x/)
+ 2. [Flask-RESTful](https://flask-rescful.readthedocs.io/en/latest/)
+ 3. [flask-mongoengine](https://docs.mongoengine.org/projects/flask-mongoengine/en/latest/)
+
+ <b> Flask </b> is a framework that allows us to easily setup a web server with Python. It's a bit minimalistic, so to make it work better for our needs we add Flask-RESTful, which introduces a lot of helpful methods to handle creating a REST API. Flask-mongoengine is the same as the mongoengine we've used previously; it just adds a little more code to integrate mongoengine with our Flask app.
+ ## Routing 
+By default, Flask supports routing with '@' decorators
+```
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+```
+
+This means that our server will return 'Hello World!' whenever we make an GET call on localhost:5000/
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+app = Flask(__name__)
+api = Api(app)
+rider_data = {'R0': 'Karim', 'R1': 'Manuja', 'R2': 'Phil'}  # Contains ID:Name of riders.
+
+
+# Resource supporting GET method with no parameters.
+class SampleResource(Resource):
+    def get(self):
+        sample_response = "SampleResource got GET request."
+        return sample_response
+
+
+# Concrete example of RiderResource supporting GET method with rider ID query parameter.
+class RiderResource(Resource):
+    def get(self):
+        get_parser = reqparse.RequestParser()  # From flask library.
+        get_parser.add_argument('id', type=str)  # Expect a query param called "id". Replicate this line for each query param.
+        args = get_parser.parse_args()  # Stores value of "id" (and any other query params), defaults to None.
+        if args.id in rider_data:  # Check if "id" exists.
+            sample_response = "Rider with ID *%s* is *%s*" % (args.id, rider_data[args.id])
+        else:  # Provided "id" doesn't exist, return a useful message.
+            sample_response = "Rider ID *%s* doesn't exist, legal values are: %s"\
+                              % (args.id, list(rider_data.keys()))
+        return sample_response
+
+
+# http://localhost:5000/sample_route
+api.add_resource(SampleResource, '/sample_route')  # add_resource method maps a url/route to a resource.
+
+# http://localhost:5000/rider
+# http://localhost:5000/rider?id=R5
+api.add_resource(RiderResource, '/rider')
+
+
+if __name__ == "__main__":
+    app.run()  # Runs web app @ http://localhost:5000 by default for me.
